e2c421510318541f53817b596ffa0fe2995cb21d
Johan Satgé <johansatge@users.noreply.github.com>
2017-03-25 10:40:56 +0100

    Better lazyload

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,47 +1,78 @@
-(function(window, document)
-{
+(function(window, document) {
 
   var module = {}
+  var isLazyLoadDone = false
 
   module.init = function() {
     var search = document.querySelector('.js-search');
     search.addEventListener('input', _onSearch);
     document.querySelector('.js-sort-select').addEventListener('change', _onSort);
-    document.addEventListener('scroll', _onScroll);
+    window.addEventListener('scroll', _onLazyLoad);
+    window.addEventListener('resize', _onLazyLoad);
 
     _onSort();
-    _onScroll();
+    _onLazyLoad();
     search.focus();
   };
 
   var _onSearch = function(evt) {
     var search = evt.currentTarget.value.toLowerCase();
+    var noResults = document.querySelector('.js-items-noresults');
+    var visibleItems = 0;
     [].forEach.call(document.querySelectorAll('.js-item'), function(item) {
       var itemValue = item.getAttribute('data-search');
-      item.style.display = search.length === 0 || itemValue.search(search) > -1 ? 'inline-block' : 'none';
+      if (search.length === 0 || itemValue.search(search) > -1) {
+        item.style.display = 'inline-block';
+        visibleItems += 1;
+      }
+      else {
+        item.style.display = 'none';
+      }
     });
+    noResults.style.display = visibleItems === 0 ? 'block' : 'none';
+    if (!isLazyLoadDone) {
+      _onLazyLoad()
+    }
   };
 
   var _onSort = function() {
     var select = document.querySelector('.js-sort-select');
     var label = document.querySelector('.js-sort-label');
     label.innerHTML = select.options[select.selectedIndex].innerHTML;
-  }
+    if (!isLazyLoadDone) {
+      _onLazyLoad()
+    }
+  };
 
-  var _onScroll = function(evt) {
-    var windowHeight = window.innerHeight;
+  var _onLazyLoad = function() {
     var images = document.querySelectorAll('.js-lazy[data-src-lazy]');
     if (images.length === 0) {
-      document.removeEventListener('scroll', _onScroll);
+      isLazyLoadDone = true
+      window.removeEventListener('scroll', _onLazyLoad);
+      window.removeEventListener('resize', _onLazyLoad);
       return;
     }
+    var windowHeight = window.innerHeight;
     [].forEach.call(images, function(image) {
       var boundings = image.getBoundingClientRect();
-      if (boundings.top >= 0 && boundings.top < windowHeight) {
+      var needsLazyLoad = false;
+      if (boundings.top > 0 && boundings.top < windowHeight) {
+        needsLazyLoad = true
+      }
+      if (boundings.top + boundings.height > 0 && boundings.top + boundings.height < windowHeight) {
+        needsLazyLoad = true
+      }
+      if (needsLazyLoad) {
+        image.addEventListener('load', _onLazyLoaded);
         image.src = image.getAttribute('data-src-lazy')
         image.removeAttribute('data-src-lazy')
       }
     });
+  }
+
+  var _onLazyLoaded = function() {
+    this.classList.remove('js-lazy-hidden');
+    this.removeEventListener('load', _onLazyLoaded);
   }
 
   window.App = module;
