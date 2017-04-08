3300ec09fc85aa919e79502b82a7b3c2eb8846b1
Johan Satgé <johansatge@users.noreply.github.com>
2017-04-08 13:21:02 +0200

    Add Fidgetry, Noblespin new models, VC EDC, Stealth Spinners

--- src/build_thumbnails.php
+++ src/build_thumbnails.php
@@ -10,7 +10,12 @@ foreach($argv as $argv)
   {
     if (preg_match('#_thumb\.jpg$#', $model)) continue;
     if (preg_match('#_thumb\@2x.jpg$#', $model)) continue;
-    echo 'Generating ' . $model . "\n";
+    if (is_readable(str_replace('.jpg', '_thumb.jpg', $model)))
+    {
+      echo 'Skipping ' . $model . "\n";
+      continue;
+    }
+    optimize_image($model);
     build_thumbnail($model, str_replace('.jpg', '_thumb.jpg', $model), 200, 200);
     build_thumbnail($model, str_replace('.jpg', '_thumb@2x.jpg', $model), 400, 400);
   }
@@ -18,9 +23,18 @@ foreach($argv as $argv)
 
 function build_thumbnail($source_path, $dest_path, $dest_width, $dest_height)
 {
+  echo 'Generating ' . $dest_path . "\n";
   list($source_width, $source_height) = getimagesize($source_path);
   $base_image = imagecreatefromjpeg($source_path);
   $resized_image = imagecreatetruecolor($dest_width, $dest_height);
   imagecopyresampled($resized_image, $base_image, 0, 0, 0, 0, $dest_width, $dest_height, $source_width, $source_height);
   imagejpeg($resized_image, $dest_path, 100);
+  optimize_image($dest_path);
+}
+
+function optimize_image($path)
+{
+  echo 'Optimizing ' . $path;
+  exec('jpegoptim ' . $path . ' --max=82 --strip-all', $output);
+  echo ' (' . round(filesize($path) / 1000) . 'Kb)' . "\n";
 }
